32a8450cfa911adbd63a977caeb7152c73d765cb
aljohnston112 <aljohnston112@gmail.com>
2023-08-14 19:40:22 -0500

    Added comments to the card view state

--- PokemonTCG/Models/CardViewState.cs
+++ PokemonTCG/Models/CardViewState.cs
@@ -6,27 +6,40 @@
     /// </summary>
     class CardViewState
     {
-        internal readonly string ImagePath;
-        internal readonly bool CanMakeActive;
-        internal readonly bool CanAddToBench;
-        internal readonly bool CanAttachToPokemon;
-        internal readonly bool CanUse;
+       
 
         internal CardViewState(
-            string imagePath, 
-            bool canMakeActive, 
-            bool canAddToBench,
-            bool canAttachToPokemon,
-            bool canUse
+           
             )
         {
-            ImagePath = imagePath;
-            CanMakeActive = canMakeActive;
-            CanAddToBench = canAddToBench;
-            CanAttachToPokemon = canAttachToPokemon;
-            CanUse = canUse;
+            
         }
 
     }
 
 }
+
+/* Pokemon
+ *      Use ability - If ability condition is met
+ *          Bench
+ *          Active
+ *      Attack - If energy condition is met and attack condition is met
+ *          Active
+ *      Put on bench - "If room on bench and is a basic Pokemon" or "All 4 cards of a V-UNION (from the discard pile)".
+ *          Hand
+ *      Evolve - If evolution is available in hand and evolves from the Pokemon to be evolved
+ *          Active
+ *          Bench
+ *      Retreat - If retreat cost is met
+ *          Active
+ *      Make active - If start of game or active was knocked out
+ *          Hand
+ *          Bench
+ *      Attach energy - If not done
+ *          Active
+ *          Bench
+ *          
+ * Trainer - If trainer condition is met
+ *      Use
+ * 
+ */
--- PokemonTCG/ViewModel/CardStateViewModel.cs
+++ PokemonTCG/ViewModel/CardStateViewModel.cs
@@ -1,5 +1,8 @@
 ﻿namespace PokemonTCG.View
 {
+
+
+
     internal class CardStateViewModel
     {
 
